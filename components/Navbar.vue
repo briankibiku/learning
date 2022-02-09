d9fa61c76d8a358e42ad90e06ca8ff7b328cfa24
kibiku <kibikukimani2@gmail.com>
2022-02-09 15:16:26 +0300

    feat: retrun nav bar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,31 @@
 <template>
-  <b-navbar toggleable="lg" type="light" variant="light">
-    <b-navbar-brand>
-      <img
-        src="https://drive.google.com/uc?export=view&id=1EztWl51HpYGlZ1G3hD5iPF6z6YgeGq9n"
-        alt=""
-        width="140"
-        height="60"
-        class="d-inline-block align-text-top"
-      />
-    </b-navbar-brand>
-  </b-navbar>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="/">
+        <img
+          src="https://drive.google.com/uc?export=view&id=1EztWl51HpYGlZ1G3hD5iPF6z6YgeGq9n"
+          alt=""
+          width="140"
+          height="60"
+          class="d-inline-block align-text-top"
+      /></b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item href="/about">About</b-nav-item>
+            <b-nav-item href="/contact">Contact</b-nav-item>
+            <b-nav-item href="/hire">Hire</b-nav-item>
+            <b-nav-item href="/mountains">Mountains</b-nav-item>
+            <b-nav-item href="/projects">Projects</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <style>
 /* home route and active route will show in bold as it matches / and /about */
